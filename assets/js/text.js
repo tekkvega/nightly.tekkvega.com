e949032d2820ddf42517c9f660baa6c0b808c0ff
Mott's Applesauce <josiahvega123@gmail.com>
2022-04-07 18:11:34 -0400

    aaaaaaaaaaaaaaaaaa

---
+++ assets/js/text.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('send_btn');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_i947pte';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
